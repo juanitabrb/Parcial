eb921b8d32da26652620c4010e3ad3a87b035b86
Juanita Buriticá Rodríguez <juanita.buriticaro@autonoma.edu.co>
2021-10-27 09:49:52 -0500

    El editar muestra las respuestas registradas previamente

--- resources/views/dashboard/question/form_fields.blade.php
+++ resources/views/dashboard/question/form_fields.blade.php
@@ -7,7 +7,7 @@
           esta será más fácil de mantener. Si un módulo agrupa funcionalidades muy dispersas,
            su cohesión es baja, si un módulo tiene una única funcionalidad, su cohesión es alta.
         </label>
-        <input type="text" class="form-control mb-3" name ="respuesta1" id="respuesta1">
+        <input type="text" class="form-control mb-3" name ="respuesta1" id="respuesta1" value="{{$question->respuesta1}}">
       
         <div class="form-group">
             <h4>Pregunta 2</h4>
@@ -18,7 +18,7 @@
                   Actualmente el código de HealthService tiene referencias directas a AuthenticationService.
                   ¿Qué cambio deberíamos hacer para cumplir con el Dependency Inversion Principle?
             </label>
-            <input type="text" class="form-control mb-3" name ="respuesta2" id="respuesta2">
+            <input type="text" class="form-control mb-3" name ="respuesta2" id="respuesta2" value="{{$question->respuesta2}}">
         </div>
 
         <div class="form-group">
@@ -29,7 +29,7 @@
                 las cuales no deben depender de los detalles, son estos los que dependen de las abstracciones.
                  Normalmente esto aplica en una arquitectura de 3 capas. La imagen que mejor representa esta afirmación es:
             </label>
-            <input type="text" class="form-control mb-3" name ="respuesta3" id="respuesta3">
+            <input type="text" class="form-control mb-3" name ="respuesta3" id="respuesta3" value="{{$question->respuesta3}}">
         </div>
 
         <div class="form-group">
@@ -40,7 +40,7 @@
                 Recuerda el principio que menciona que los clientes NO DEBEN estar forzados a depender de métodos que 
                 ellos no utilizan:
             </label>
-            <input type="text" class="form-control mb-3" name ="respuesta4"  id="respuesta4">
+            <input type="text" class="form-control mb-3" name ="respuesta4"  id="respuesta4" value="{{$question->respuesta4}}">
         </div>
 
         <div class="form-group">
@@ -50,7 +50,7 @@
                 Cada vez que se introduce tipo de Mail nuevo modificamos la clase MailSender para adaptar el envío a este nuevo tipo de email.
                 ¿Qué principio SOLID estamos violando?
             </label>
-            <input type="text" class="form-control mb-3" name ="respuesta5" id="respuesta5">
+            <input type="text" class="form-control mb-3" name ="respuesta5" id="respuesta5" value="{{$question->respuesta5}}">
         </div>
 
         <div class="form-group">
@@ -59,7 +59,7 @@
                 El Dependency Inversion Principle recomienda que los módulos de alto nivel no dependan de los de bajo nivel.
                 ¿Qué estrategia usa para conseguirlo?
             </label>
-            <input type="text" class="form-control mb-3" name ="respuesta6"  id="respuesta6">
+            <input type="text" class="form-control mb-3" name ="respuesta6"  id="respuesta6" value="{{$question->respuesta6}}">
         </div>
 
         <div class="form-group">
@@ -69,7 +69,7 @@
                 deben ser abiertas para la modificación y cerradas para la extensión. 
                 Siendo un sistema modificable, sin extender el código existente, evitando errores.
             </label>
-            <input type="text" class="form-control mb-3" name ="respuesta7" id="respuesta7">
+            <input type="text" class="form-control mb-3" name ="respuesta7" id="respuesta7" value="{{$question->respuesta7}}">
         </div>
 
         <div class="form-group">
@@ -78,7 +78,7 @@
                 Determina el caso de violación a que principio pertenece: 
                 En un mismo método mezclamos lógica del negocio, con lógica de presentación.
             </label>
-            <input type="text" class="form-control mb-3" name ="respuesta8" id="respuesta8">
+            <input type="text" class="form-control mb-3" name ="respuesta8" id="respuesta8" value="{{$question->respuesta8}}">
         </div>
 
         <div class="form-group">
@@ -87,7 +87,7 @@
                 Si S es un subtipo de T que queremos introducir de forma segura en el programa. 
                 ¿Qué requiere el principio de Liskov que cumpla S?
             </label>
-            <input type="text" class="form-control mb-3" name ="respuesta9" id="respuesta9">
+            <input type="text" class="form-control mb-3" name ="respuesta9" id="respuesta9" value="{{$question->respuesta9}}">
         </div>
 
         <div class="form-group">
@@ -95,7 +95,7 @@
             <label for="respuesta10">
                 ¿Qué método no debería formar parte de la clase Cesta según el Single Responsibility Principle?
             </label>
-            <input type="text" class="form-control mb-3" name ="respuesta10"  id="respuesta10">
+            <input type="text" class="form-control mb-3" name ="respuesta10"  id="respuesta10" value="{{$question->respuesta10}}">
         </div>
 
 
